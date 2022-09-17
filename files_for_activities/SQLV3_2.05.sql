2f7f0ffef648ffe3e76b2a25fb032fdb242dbe50
Manuel buendía <manuelbuendiagg@gmail.com>
2022-09-17 19:15:24 +0200

    SQL 2-05 and 2.06

--- files_for_activities/SQLV3_2.05.sql
+++ files_for_activities/SQLV3_2.05.sql
@@ -1,49 +1,55 @@
 -- 1 Select all the actors with the first name ‘Scarlett’
-use sakila;
 SELECT * FROM sakila.actor
 WHERE first_name = 'Scarlett';
 
 -- 2 How many films (movies) are available for rent and how many films have been rented?
-SELECT COUNT(inventory_id) FROM sakila.inventory;
-SELECT COUNT(rental_id) FROM sakila.rental;
+SELECT inventory_id as available, count(*) from sakila.inventory;
+SELECT rental_id as films_rented, count(*) from sakila.rental;
 
 -- 3 What are the shortest and longest movie duration? Name the values max_duration and min_duration.
-SELECT MAX(length) FROM sakila.film;
-SELECT MIN(length) FROM sakila.film;
+SELECT MAX(length) as Max_length, MIN(Length) as Min_length FROM sakila.film;
 
 -- 4 What's the average movie duration expressed in format (hours, minutes)?
-SELECT AVG(length) FROM sakila.film;
-
+SELECT sec_to_time(AVG(length)) FROM sakila.film;
 -- 5 How many distinct (different) actors' last names are there?
-select distinct (last_name) from sakila.actor;
+SELECT last_name FROM sakila.actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
 
 -- 6 Since how many days has the company been operating (check DATEDIFF() function)?
-SELECT min(rental_date) FROM sakila.rental;
-select max(rental_date) FROM sakila.rental;
-SELECT DATEDIFF("2006-02-14","2005-05-24");
+SELECT min(rental_date) as min, max(rental_Date) as max, DATEDIFF('min','max') as date FROM sakila.rental;
+
 
 -- 7 Show rental info with additional columns month and weekday. Get 20 results.
-SELECT rental_date ,monthname(rental_date) as "monthinfo"
+SELECT * , monthname(rental_date) as month, dayname(rental_date) as day
 FROM sakila.rental
-LIMIT 10;
-SELECT rental_date ,DAYNAME(rental_date) as "day_info"
-FROM sakila.rental;
-LIMIT 10;
+LIMIT 20;
+
 -- 8 Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
-SELECT rental_date ,DAYNAME(rental_date) as "day_type"
+SELECT *, weekday(rental_date),
+CASE 
+WHEN weekday(rental_date) = '0' then 'workday'
+WHEN weekday(rental_date) = '1' then 'workday'
+WHEN weekday(rental_date) = '2' then 'workday'
+WHEN weekday(rental_date) = '3' then 'workday'
+WHEN weekday(rental_date) = '4' then 'workday'
+WHEN weekday(rental_date) = '5' then 'workday'
+ELSE 'weekend'
+END AS 'day type'
 FROM sakila.rental;
 
 -- 9 Get release years
-SELECT release_year FROM sakila.film;
+SELECT release_year FROM sakila.film
+GROUP BY release_year;
 
 -- 10 Get all films with ARMAGEDDON in the title.
 SELECT * FROM sakila.film
-WHERE title = "ARMAGEDDON";
+WHERE title = 'ARMAGEDDON';
 
 -- 11 Get all films which title ends with APOLLO.
 SELECT *
 FROM sakila.film
-WHERE title LIKE "%APOLO";
+WHERE title LIKE "APOLLO%";
 
 -- 12 Get 10 the longest films.
 SELECT * FROM sakila.film
@@ -53,6 +59,3 @@ LIMIT 10;
 -- 13 How many films include Behind the Scenes content?
 SELECT COUNT(*) FROM sakila.film
 WHERE special_features LIKE "%Behind the Scenes"
-
-
-
--- files_for_activities/SQLV3_2.06.sql
+++ files_for_activities/SQLV3_2.06.sql
@@ -1,41 +1,45 @@
 -- 1 In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list
 SELECT last_name FROM sakila.actor
 GROUP BY last_name
-HAVING COUNT(1) = 1
+HAVING COUNT(*) = 1;
 
 -- 2 Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
 SELECT last_name FROM sakila.actor
 GROUP BY last_name
-HAVING count(last_name) > 1;
+HAVING count(*) > 1;
 
 -- 3 Using the rental table, find out how many rentals were processed by each employee.
-SELECT staff_id, COUNT(*) staff_id
+SELECT staff_id, COUNT(*)
 FROM sakila.rental
-GROUP BY staff_id
-HAVING COUNT(staff_id);
+GROUP BY staff_id;
+
 
 -- 4 Using the film table, find out how many films were released each year.
-SELECT release_year, COUNT(*) release_year
+SELECT release_year, COUNT(*) as num_films
 FROM sakila.film
 GROUP BY release_year
-HAVING COUNT(release_year);
+ORDER BY release_year;
 
 -- 5 Using the film table, find out for each rating how many films were there.
-SELECT rating, COUNT(*) rating
+SELECT rating, COUNT(*) as num_films
 FROM sakila.film
-GROUP BY rating
-HAVING COUNT(rating);
+GROUP BY rating;
+
 
 -- 6 What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
-SELECT rating, AVG(length)  AS "mean"
+SELECT rating, ROUND(AVG(length),2)  AS avg_duration
 FROM sakila.film
 GROUP BY rating
-HAVING COUNT(rating);
+ORDER BY avg_duration desc;
 
 -- 7 Which kind of movies (rating) have a mean duration of more than two hours?
--- just PG-13
+select rating, round(avg(length),2) as avg_duration from sakila.film
+group by rating
+having avg_duration > 120;
+
 
 -- 8 Rank films by length (filter out the rows that have nulls or 0s in length column). In your output, only select the columns title, length, and the rank.
-SELECT title, length FROM sakila.film
+SELECT title, length
+FROM sakila.film
+WHERE length != 0 AND length IS NOT NULL
 ORDER BY length DESC;
-
